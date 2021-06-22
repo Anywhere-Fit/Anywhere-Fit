719ce8f4418fe12c36fd3f7bd7f2abbacf82b78f
Zach-Morris <zach-morris@lambdastudents.com>
2021-06-22 15:24:39 -0400

    Routers

--- api/server.js
+++ api/server.js
@@ -7,4 +7,27 @@ server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+const usersRouter = require('./users/users-router')
+const classesRouter = require('./classes/classes-router')
+
+server.use("/api/users",usersRouter);
+server.use("/api/classes", classesRouter);
+
+
+//SANITY CHECK ENDPOINT
+server.get("/", (req, res, next)=>{
+    res.json({
+        message: "API Up"
+    })
+})
+
+
+//Global Error Handling 
+server.use((err, req, res, next)=>{
+    res.json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
 module.exports = server
